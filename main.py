f183fbe3210c96d6ab6980b46c84dbb17aadba77
Dr. James Burke <burkej@sbregional.org>
2022-01-14 10:27:17 -0500

    Added velocity-style movement

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def on_up_pressed():
+    the_angle = shipangle*Math.PI/4
+    shipsprite.set_velocity(Math.cos(the_angle)*5, Math.sin(the_angle)*5)
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+def on_left_pressed():
+    global shipangle
+    shipangle += -1
+    shipangle = shipangle % 8
+    if shipangle < 0:
+        shipangle = 8 + shipangle
+    shipsprite.set_image(ships[shipangle])
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
 def gtp(position: number, isx: bool):
     global coord
     if isx:
@@ -5,10 +19,21 @@ def gtp(position: number, isx: bool):
     else:
         coord = Math.sin(Math.PI / 4 * position) * 8 + 8
     return coord
+
+def on_right_pressed():
+    global shipangle
+    shipangle += 1
+    shipangle = shipangle % 8
+    shipsprite.set_image(ships[shipangle])
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
 coord = 0
+shipsprite: Sprite = None
+shipangle = 0
+ships: List[Image] = []
 oneship: Image = None
-ships = []
 coord2 = 0
+
 for index in range(8):
     oneship = img("""
         . . . . . . . . . . . . . . . . 
@@ -28,7 +53,6 @@ for index in range(8):
                 . . . . . . . . . . . . . . . . 
                 . . . . . . . . . . . . . . . .
     """)
-    oneship.draw_rect(0, 0, 16, 16, 7)
     oneship.draw_line(gtp(index + 0, True),
         gtp(index + 0, False),
         gtp(index + 3, True),
@@ -42,4 +66,5 @@ for index in range(8):
         gtp(index + 0, False),
         1)
     ships.append(oneship)
-shipsprite = sprites.create(ships[2], SpriteKind.player)
+shipangle = 0
+shipsprite = sprites.create(ships[shipangle], SpriteKind.player)
--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    the_angle = shipangle * Math.PI / 4
+    shipsprite.setVelocity(Math.cos(the_angle) * 10 + shipsprite.vx, Math.sin(the_angle) * 10 + shipsprite.vy)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    shipangle += -1
+    shipangle = shipangle % 8
+    if (shipangle < 0) {
+        shipangle = 8 + shipangle
+    }
+    shipsprite.setImage(ships[shipangle])
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    shipangle += 1
+    shipangle = shipangle % 8
+    shipsprite.setImage(ships[shipangle])
+})
 function gtp (position: number, isx: boolean) {
     if (isx) {
         coord = Math.cos(Math.PI / 4 * position) * 8 + 8
@@ -7,6 +24,9 @@ function gtp (position: number, isx: boolean) {
     return coord
 }
 let coord = 0
+let the_angle = 0
+let shipsprite: Sprite = null
+let shipangle = 0
 let ships: Image[] = []
 let oneship: Image = null
 let coord2 = 0
@@ -29,15 +49,11 @@ for (let index = 0; index <= 7; index++) {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `
-    oneship.drawRect(0, 0, 16, 16, 7)
     oneship.drawLine(gtp(index + 0, true), gtp(index + 0, false), gtp(index + 3, true), gtp(index + 3, false), 1)
     oneship.drawLine(gtp(index + 3, true), gtp(index + 3, false), 7, 7, 1)
     oneship.drawLine(7, 7, gtp(index + 5, true), gtp(index + 5, false), 1)
     oneship.drawLine(gtp(index + 5, true), gtp(index + 5, false), gtp(index + 0, true), gtp(index + 0, false), 1)
     ships.push(oneship)
 }
-let shipsprite = sprites.create(ships[2], SpriteKind.Player)
-for (let index = 0; index <= 7; index++) {
-    shipsprite.setImage(ships[index])
-    pause(500)
-}
+shipangle = 0
+shipsprite = sprites.create(ships[shipangle], SpriteKind.Player)
